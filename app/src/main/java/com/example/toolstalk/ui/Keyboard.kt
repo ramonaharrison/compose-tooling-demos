ba74533cf202adc209fd28cc5c98c8a14f3cef8c
Ramona Harrison <ramona.harrison@nytimes.com>
2022-07-28 16:18:41 -0400

    add keyboard

--- app/src/main/java/com/example/toolstalk/ui/Keyboard.kt
+++ app/src/main/java/com/example/toolstalk/ui/Keyboard.kt
@@ -1,10 +1,105 @@
 package com.example.toolstalk.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.toolstalk.model.AnswerState
 
 @Composable
-fun Keyboard(modifier: Modifier = Modifier) {
-    Box(modifier = modifier)
+fun Keyboard(
+    keyboardState: Map<Letter, AnswerState>,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row {
+            "QWERTYUIOP".forEach {
+                Key(
+                    symbol = Letter(it.toString()),
+                )
+            }
+        }
+        Row {
+            "ASDFGHJKL".forEach {
+                Key(
+                    symbol = Letter(it.toString()),
+                )
+            }
+        }
+        Row {
+            Key(
+                symbol = Enter,
+                modifier = Modifier.weight(2f)
+            )
+            "ZXCVBNM".forEach {
+                Key(
+                    symbol = Letter(it.toString()),
+                )
+            }
+            Key(
+                symbol = Backspace,
+                modifier = Modifier.weight(2f)
+            )
+        }
+    }
 }
+
+@Composable
+fun Key(
+    symbol: Symbol,
+    modifier: Modifier = Modifier,
+    color: Color = Color.LightGray,
+    onClick: (Symbol) -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .padding(2.dp)
+            .background(color = color)
+            .clickable { onClick(symbol) }
+            .padding(8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        when (symbol) {
+            is Backspace -> {
+                Text(
+                    text = "←",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 16.sp,
+                )
+            }
+            is Enter -> {
+                Text(
+                    text = "Enter",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 16.sp,
+                )
+            }
+            is Letter -> {
+                Text(
+                    text = symbol.value,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 16.sp,
+                )
+            }
+        }
+
+    }
+}
+
+sealed class Symbol
+
+data class Letter(val value: String) : Symbol()
+object Enter : Symbol()
+object Backspace : Symbol()
