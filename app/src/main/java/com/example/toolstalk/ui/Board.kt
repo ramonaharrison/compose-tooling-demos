585659a6cd221408c0a2b0f362ffd76310fe6a06
Ramona Harrison <ramona.harrison@nytimes.com>
2022-08-26 15:40:26 -0400

    add preview parameter provider

--- app/src/main/java/com/example/toolstalk/ui/Board.kt
+++ app/src/main/java/com/example/toolstalk/ui/Board.kt
@@ -2,12 +2,13 @@ package com.example.toolstalk.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.example.toolstalk.model.*
 
@@ -24,11 +25,20 @@ fun Board(modifier: Modifier = Modifier, boardState: BoardState) {
     }
 }
 
+class BoardStatePreviewParameterProvider : PreviewParameterProvider<BoardState> {
+    override val values = sequenceOf(
+        listOf("BOAR", "", "", "", "", "").toBoardState("TOOLS"),
+        listOf("BOARD", "TOPIC", "TILES", "TOOL", "", "").toBoardState("TOOLS"),
+        listOf("BOARD", "TOPIC", "TILES", "TRIAL", "TOOLS", "").toBoardState("TOOLS"),
+    )
+}
 
 @Preview
 @Composable
-fun BoardPreview() {
+fun BoardPreview(
+    @PreviewParameter(BoardStatePreviewParameterProvider::class) boardState: BoardState,
+) {
     Surface {
-        Board(boardState = sampleBoard1)
+        Board(boardState = boardState)
     }
 }
