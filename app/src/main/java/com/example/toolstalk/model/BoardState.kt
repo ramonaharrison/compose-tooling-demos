7b647703fe4aea44f5323ae3fa28f177ade82acf
Ramona Harrison <ramona.harrison@nytimes.com>
2022-08-31 11:13:58 -0400

    add play letter to view model

--- app/src/main/java/com/example/toolstalk/model/BoardState.kt
+++ app/src/main/java/com/example/toolstalk/model/BoardState.kt
@@ -6,11 +6,18 @@ import com.example.toolstalk.ui.theme.*
 
 data class BoardState(
     val rowStates: List<RowState> = List(6) { RowState() }
-)
+) {
+    val isSolved = rowStates.any { row -> row.isCorrectWord }
+
+    fun letterAtPosition(position: Position): String? = rowStates[position.y].tileStates[position.x].letter
+}
 
 data class RowState(
     val tileStates: List<TileState> = List(5) { TileState() }
-)
+) {
+    val isCorrectWord =
+        tileStates.filter { tile -> tile.answerState == AnswerState.CORRECT }.size == 5
+}
 
 data class TileState(
     val letter: String? = null,
@@ -32,3 +39,27 @@ data class KeyboardState(
     val keys: Map<Letter, AnswerState> =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ".associate { Letter(it.toString()) to AnswerState.PENDING }
 )
+
+data class Position(
+    val x: Int = 0,
+    val y: Int = 0
+) {
+    val isEndOfRow = x == 4
+
+    fun nextPosition() = if (isEndOfRow) {
+        this
+    } else Position(x + 1, y)
+
+    fun previousPosition() = if (x == 0) {
+        this
+    } else {
+        Position(x - 1, y)
+    }
+
+    fun moveToNextRow() = if (y == 5) {
+        this
+    } else {
+        Position(0, this.y + 1)
+    }
+}
+
--- app/src/main/java/com/example/toolstalk/ui/GameScreenViewModel.kt
+++ app/src/main/java/com/example/toolstalk/ui/GameScreenViewModel.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import com.example.toolstalk.model.BoardState
 import com.example.toolstalk.model.KeyboardState
+import com.example.toolstalk.model.Position
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -20,17 +21,50 @@ class GameScreenViewModel @Inject constructor(
     private var _keyboardState = MutableStateFlow(KeyboardState())
     val keyboardState = _keyboardState.asStateFlow()
 
+    private var _position = MutableStateFlow(Position())
+
     fun playSymbol(symbol: Symbol) {
+        val board = boardState.value
+        if (board.isSolved) return
+
+        val position = _position.value
+
         when (symbol) {
             is Letter -> {
-                Toast.makeText(context, symbol.value, Toast.LENGTH_SHORT).show()
+                if (position.isEndOfRow && board.letterAtPosition(position) != null) {
+                    // At end of row until user presses enter
+                    return
+                } else {
+                    playLetter(board, position, symbol.value)
+                }
             }
             is Enter -> {
                 Toast.makeText(context, "Enter!", Toast.LENGTH_SHORT).show()
             }
             is Backspace -> {
-                Toast.makeText(context, "Backspace!", Toast.LENGTH_SHORT).show()
+                playLetter(board, position, null)
             }
         }
+    }
+
+    private fun playLetter(board: BoardState, position: Position, letter: String?) {
+        val currentRow = board.rowStates[position.y]
+        val updatedTile = currentRow.tileStates[position.x].copy(
+            letter = letter
+        )
+        val updatedRow = currentRow.copy(
+            tileStates = currentRow.tileStates.toMutableList()
+                .apply { this[position.x] = updatedTile }
+        )
+        val updatedRowStates =
+            board.rowStates.toMutableList().apply { this[position.y] = updatedRow }
+
+        val newPosition = if (letter == null) {
+            position.previousPosition()
+        } else {
+            position.nextPosition()
+        }
+        this._position.value = newPosition
+        this._boardState.value = board.copy(rowStates = updatedRowStates)
     }
 }
